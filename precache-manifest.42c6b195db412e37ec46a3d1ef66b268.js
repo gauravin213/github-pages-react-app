6ec67e0522fa8edbd79df69b47270a25b9af897d
gaurav <gauravin213@gmail.com>
2020-06-20 22:18:51 +0530

    Updates

---
+++ precache-manifest.42c6b195db412e37ec46a3d1ef66b268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d50caa17523f17ee2b2bd9f0d7595f5",
+    "url": "/github-pages-react-app/index.html"
+  },
+  {
+    "revision": "135605d53ffa948e197d",
+    "url": "/github-pages-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e8bde12835a1141f542",
+    "url": "/github-pages-react-app/static/js/2.b5bb0ed9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-react-app/static/js/2.b5bb0ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135605d53ffa948e197d",
+    "url": "/github-pages-react-app/static/js/main.3ba465e4.chunk.js"
+  },
+  {
+    "revision": "77e79709d8c59ca16367",
+    "url": "/github-pages-react-app/static/js/runtime-main.9a843c45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
